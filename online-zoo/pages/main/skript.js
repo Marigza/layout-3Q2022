f4c2d4e52fe5e9643debf4ab7f5b91dac05f668b
Marigza <marigola@mail.ru>
2022-10-07 21:15:26 +0300

    feat: create and link js-files

---
+++ online-zoo/pages/main/skript.js
@@ -0,0 +1,30 @@
+// burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    const modal = document.querySelector('.modal_container');
+    const menuCloseItem = document.querySelector('.header_nav-close');
+    const modalCloseItem = document.querySelector('.modal_close');
+    const menuLinks = document.querySelectorAll('.header_link');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+        modal.classList.add('modal_container_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+        modal.classList.remove('modal_container_active');
+    });
+    if (window.innerWidth <= 410) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header_nav_active');
+                modal.classList.remove('modal_container_active');
+            });
+        }
+    }
+    modalCloseItem.addEventListener('click', () => {
+        modal.classList.remove('modal_container_active');
+        menu.classList.remove('header_nav_active');
+    });
+}());
